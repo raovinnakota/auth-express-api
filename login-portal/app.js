96070a6fc56862cc80239e88fdf2451c51707ca4
raobvinnakota <raobvinnakota@ibm.com>
2019-07-24 17:52:05 -0400

    Authorization session handled

--- login-portal/app.js
+++ login-portal/app.js
@@ -1,8 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +17,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -26,6 +27,13 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+//express-session
+app.use(session({
+  secret: KSKGJkskdi35u7593823jdnn3279f87vdshnqkhKHLKHLKHNkinaIO,
+  resave: true,
+  saveUninitialized: false
+}));
 
 // error handler
 app.use(function(err, req, res, next) {
