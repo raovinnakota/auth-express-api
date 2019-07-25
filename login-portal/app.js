92427cc82f8ab05927c97695fa8313c15e6e740e
raobvinnakota <raobvinnakota@ibm.com>
2019-07-25 16:12:23 -0400

    Authentication enabled

--- login-portal/app.js
+++ login-portal/app.js
@@ -1,14 +1,38 @@
 var createError = require('http-errors');
 var express = require('express');
+
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
 const dashboardRouter = require("./routes/dashboard");         
 const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 
 var app = express();
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-379498.okta.com/',
+  token: '00a1HModIsWW02VWzz-RejjoPRKoGw_0QNaCv5p70h'
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-379498.okta.com/oauth2/default",
+  client_id: "0oazt7ynrU98cwGu2356",
+  client_secret: "y5WIWfX-G6u8piB-DyFxRHK-RVGHIbhnyy1g_DhM",
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +45,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', publicRouter);
-app.use('/dashboard', dashboardRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,6 +59,11 @@ app.use(session({
   resave: true,
   saveUninitialized: false
 }));
+app.use(oidc.router);
+
+app.get('/test', (req, res) => {
+  res.json({ profile: req.user ? req.user.profile : null });
+});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -45,5 +75,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
 
 module.exports = app;
